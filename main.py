d291940e2ab078219a8756d422eb90db07c77ef1
Jan Schneider <st117301@stud.uni-stuttgart.de>
2019-07-26 00:25:56 +0200

    Adjusted database host name

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ AREA_TYPES_DOCUMENT_FILE = "../area-types/area_types.json"
 AREA_TYPES_SCHEMA_FILE = "../area-types/area_types_schema.json"
 
 # Database settings
-DATABASE_HOST = "localhost"
+DATABASE_HOST = ""
 DATABASE_NAME = "gis"
 DATABASE_USER = "postgres"
 DATABASE_PASSWORD = None
