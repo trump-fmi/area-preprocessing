3080e8dd9975228ecf647efca96a8b28266b5200
Jan Schneider <st117301@stud.uni-stuttgart.de>
2019-07-27 01:51:08 +0200

    Made input data file more generic

--- ExtractionRule.py
+++ ExtractionRule.py
@@ -4,7 +4,7 @@ import json
 # ********** Config **********
 PIPELINE_DIR = "pipeline"
 PIPELINE_SCRIPT_NAME = "run_pipeline.sh"
-INPUT_FILE_PATH = "../../germany-latest.osm.pbf"
+INPUT_FILE_PATH = "../../source_data.osm.pbf"
 OUTPUT_FILE_NAME = "result.json"
 GEOMETRIES_WHITELIST = ["LineString", "MultiLineString", "Polygon", "MultiPolygon"]
 TARGET_PROJECTION = 4326
