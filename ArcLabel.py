c465e0bfd63915f6bbe3ef59dc4e7ce646dadb1b
Michel Weitbrecht <>
2019-10-14 16:27:33 +0200

    Use execute_values and prepared statements to write to DB

--- ArcLabel.py
+++ ArcLabel.py
@@ -7,14 +7,3 @@ class ArcLabel:
         self.end_angle = end_angle
         self.inner_radius = inner_radius
         self.outer_radius = outer_radius
-
-    def to_sql_string(self):
-        sql_builder = []
-        sql_builder.append("NULL" if self.text is None else "'" + self.text + "'")
-        sql_builder.append("NULL" if self.center is None else "'[" + ",".join(map(str, self.center)) + "]'")
-        sql_builder.append("NULL" if self.start_angle is None else str(self.start_angle))
-        sql_builder.append("NULL" if self.end_angle is None else str(self.end_angle))
-        sql_builder.append("NULL" if self.inner_radius is None else str(self.inner_radius))
-        sql_builder.append("NULL" if self.outer_radius is None else str(self.outer_radius))
-
-        return ",".join(sql_builder)
--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2 import pool
-
+from psycopg2.extras import execute_values
 
 class DatabaseConnection:
     def __init__(self, host, database, user, password=None):
@@ -20,12 +20,15 @@ class DatabaseConnection:
         cursor.close()
         self.connection_pool.putconn(connection)
 
-    def query(self, query):
+    def write_query(self, query, template=None, query_tuples=None, page_size=1000):
         try:
             cursor = self.open_cursor()
-            cursor.execute(query)
-        except:
-            print(f"Failed query: {query}")
+            if query_tuples and template:
+                psycopg2.extras.execute_values(cursor, query, query_tuples, page_size=page_size, template=template)
+            else:
+                cursor.execute(query)
+        except Exception as e:
+            print(f"Query '{query}' failed with exception: {e} ")
             exit(-1)
         finally:
             self.close_cursor(cursor)
