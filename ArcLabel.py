bb2f673c4271042793be170eafd12ad309327574
Jan Schneider <st117301@stud.uni-stuttgart.de>
2019-10-18 21:26:49 +0200

    Fixed SQL query parameters

--- ArcLabel.py
+++ ArcLabel.py
@@ -1,9 +1,9 @@
 class ArcLabel:
 
     def __init__(self, text, center=None, start_angle=None, end_angle=None, inner_radius=None, outer_radius=None):
-        self.text = text
-        self.center = center
-        self.start_angle = start_angle
-        self.end_angle = end_angle
-        self.inner_radius = inner_radius
-        self.outer_radius = outer_radius
+        self.text = "NULL" if text is None else ("'" + self.text + "'")
+        self.center = "NULL" if center is None else ("'[" + ",".join(map(str, self.center)) + "]'")
+        self.start_angle = "NULL" if start_angle is None else str(self.start_angle)
+        self.end_angle = "NULL" if end_angle is None else str(self.end_angle)
+        self.inner_radius = "NULL" if inner_radius is None else str(self.inner_radius)
+        self.outer_radius = "NULL" if outer_radius is None else str(self.outer_radius)
