d5f17dd895c6a3d8e3e523699fb404b8db8b1fc9
Jan S <44969037+schneijan@users.noreply.github.com>
2019-10-21 16:44:29 +0200

    Removed useless quotes

--- ArcLabel.py
+++ ArcLabel.py
@@ -2,7 +2,7 @@ class ArcLabel:
 
     def __init__(self, text=None, center=None, start_angle=None, end_angle=None, inner_radius=None, outer_radius=None):
         self.text = text
-        self.center = ("'[" + ",".join(map(str, center)) + "]'") if center else None
+        self.center = ("[" + ",".join(map(str, center)) + "]") if center else None
         self.start_angle = start_angle
         self.end_angle = end_angle
         self.inner_radius = inner_radius
